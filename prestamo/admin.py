b2f78e7c807308d855e9602eb5df9b3f097a7baf
Carlos Campaña <carlos.campana.d@gmail.com>
2024-06-10 22:45:41 -0400

    arregla errores

--- prestamo/admin.py
+++ prestamo/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Estados, TipoUsuario, Usuario, Login, Libro, StockLibro, Prestamo, Renovacion, Deuda
 
-# Register your models here.
+admin.site.register(Estados)
+admin.site.register(TipoUsuario)
+admin.site.register(Usuario)
+admin.site.register(Login)
+admin.site.register(Libro)
+admin.site.register(StockLibro)
+admin.site.register(Prestamo)
+admin.site.register(Renovacion)
+admin.site.register(Deuda)
