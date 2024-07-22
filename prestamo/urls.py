1ea6cb2c5b9b6be9eda6ab39abe9e74e9121dfb6
Carlos Campaña <carlos.campana.d@gmail.com>
2024-07-21 22:13:40 -0400

    agrega vistas

--- prestamo/urls.py
+++ prestamo/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    
+    path('prestamo/', views.prestamo_libro, name='prestamo_libro'),
+    path('admin-biblioteca/', views.admin_biblioteca, name='admin_biblioteca'),
 ]
--- prestamo/views.py
+++ prestamo/views.py
@@ -9,3 +9,8 @@ def index(request):
     }
     # Renderizar la plantilla index.html con el contexto proporcionado
     return render(request, 'index.html', context)
+def prestamo_libro(request):
+    return render(request, 'prestamo_libro.html')
+
+def admin_biblioteca(request):
+    return render(request, 'admin_biblioteca.html')
